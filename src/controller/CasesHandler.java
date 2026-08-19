72355021f437edd8487c260e77b57428a0256865
Tuistmessiah <pedrocaetano90@gmail.com>
2026-08-19 11:41:49 +0200

    Added a Hello command

--- src/controller/CasesHandler.java
+++ src/controller/CasesHandler.java
@@ -15,6 +15,7 @@ import service.CaseService;
 
 /**
  * Handles onboarding case and case document HTTP endpoints.
+ * Hello form Pedro
  */
 public class CasesHandler implements HttpHandler {
     private static final Logger logger = LoggerFactory.getLogger(CasesHandler.class);
